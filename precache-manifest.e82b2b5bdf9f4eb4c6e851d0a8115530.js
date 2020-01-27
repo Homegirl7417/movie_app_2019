e4c221acd0977513fd3c794f5ba7522076396b59
Homegirl7417 <kj3477417@gmail.com>
2020-01-27 22:39:27 +0900

    Updates

---
+++ precache-manifest.e82b2b5bdf9f4eb4c6e851d0a8115530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05fb1993871e3ebc4938934386f6eef",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "56abc9f7b37e528c62b6",
+    "url": "/movie_app_2019/static/css/main.422097b4.chunk.css"
+  },
+  {
+    "revision": "2881a49c236963f006dc",
+    "url": "/movie_app_2019/static/js/2.9b0b1c14.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie_app_2019/static/js/2.9b0b1c14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56abc9f7b37e528c62b6",
+    "url": "/movie_app_2019/static/js/main.328ad32b.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
